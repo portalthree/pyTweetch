a569250ab112cc585076c5e7a36b8865613d09a7
portalthree <37633603+portalthree@users.noreply.github.com>
2021-05-10 19:58:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 import tweepy
 import requests
 from twitchAPI import Twitch
-import json
+try:
+  import ujson as json
+except ModuleNotFoundError:
+  import json
 from time import sleep
 import datetime as dt
+import pymongo
 
 CONSUMER_KEY = "CONSUMER_KEY"
 CONSUMER_SECRET = "CONSUMER_SECRET"
 ACCESS_TOKEN = "ACCESS_TOKEN"
-ACCESS_TOKEN_SECRET = "ACCESS_TOKEN_SECRET"
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m' 
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+ACCESS_TOKEN_SECRET = "ACCESS_TOKEN_SECRET" 
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -40,36 +33,40 @@ API_HEADERS = {
 }
 
 with open("streamers.json", encoding='raw_unicode_escape') as json_file:
-    content_json = json.loads(
-        json_file.read().encode('raw_unicode_escape').decode())
+    content_json = json.loads(json_file.read().encode('raw_unicode_escape').decode())
 
 data = {}
 data['streamers'] = {}
-
-def checkUser(user): #returns true if online, false if not
+    
+def checkUser(user):
     userid = twitch.get_users(logins=[user])['data'][0]['id']
     url = TWITCH_STREAM_API_ENDPOINT_V5.format(userid)
     try:
         req = requests.Session().get(url, headers=API_HEADERS)
         jsondata = req.json()
-        if 'stream' in jsondata:
-            if jsondata['stream'] is not None:
+        if('stream' in jsondata):
+            if(jsondata['stream'] is not None):
                 CURRENT_TIMESTAMP = dt.datetime.utcnow().isoformat(timespec='seconds') + 'Z'
-
 
                 data['streamers'][twitch_name] = {}
                 data['streamers'][twitch_name]['created_at'] = jsondata['stream']['created_at']
                 data['streamers'][twitch_name]['last_updated'] = CURRENT_TIMESTAMP
+                data['streamers'][twitch_name]['title'] = jsondata['stream']['channel']['status']
+                data['streamers'][twitch_name]['viewcount'] = jsondata['stream']['viewers']
+                data['streamers'][twitch_name]['game'] = jsondata['stream']['game']
+                data['streamers'][twitch_name]['followers'] = jsondata['stream']['channel']['followers']
+                data['streamers'][twitch_name]['streamid'] = jsondata['stream']['_id']
+                data['streamers'][twitch_name]['channelid'] = jsondata['stream']['channel']['_id']
+                data['streamers'][twitch_name]['name'] = jsondata['stream']['channel']['display_name']
+                #TODO: UPTIME 
 
                 with open('created_at.json', 'w') as f:
                     json.dump(data, f, indent=4)
-
 
                 with open('created_at.json', 'r') as created_at:
                     created_at_content = json.loads(created_at.read().encode('raw_unicode_escape').decode())
 
                     CREATED_AT_TIMESTAMP = created_at_content['streamers'][twitch_name]['created_at']
-
 
                     T1 = dt.datetime.strptime(CREATED_AT_TIMESTAMP, "%Y-%m-%dT%H:%M:%SZ")
                     T2 = dt.datetime.strptime(CURRENT_TIMESTAMP, "%Y-%m-%dT%H:%M:%SZ")
@@ -77,37 +74,30 @@ def checkUser(user): #returns true if online, false if not
                     difference = T2-T1
 
                     if(difference.seconds >= 300):
-                        print(bcolors.WARNING + twitch_name + "'s difference was greater than 340 seconds, did not tweet." + bcolors.ENDC)
-
-                        data['streamers'][twitch_name]['tweeted'] = "Yes"
-
-                        with open('created_at.json', 'w') as f:
-                            json.dump(data, f, indent=4)
+                        print(twitch_name + "'s difference is greater than 300 seconds, already tweeted.")
                     else:
+                        with open('created_at.json', encoding='utf-8', errors='ignore') as created_at:
+                            created_at_content = json.loads(created_at.read().encode('raw_unicode_escape').decode())
 
-                        api.update_status(status = jsondata['stream']['channel']['display_name'] + " went live!"
-                        + "\nStreaming: " + jsondata['stream']['channel']['status']
-                        + "\nhttps://twitch.tv/" + twitch_name + "\n")
-                        
-                        data['streamers'][twitch_name]['tweeted'] = "Tweeting"
-                        data['streamers'][twitch_name]['title'] = jsondata['stream']['channel']['status']
+                            api.update_status(status = jsondata['stream']['channel']['display_name'] + " went live!"
+                            + "\nStreaming: " + jsondata['stream']['channel']['status']
+                            + "\nhttps://twitch.tv/" + twitch_name + "\n")
+                                
+                            with open('created_at.json', encoding='utf-8', errors='ignore') as f:
+                                data['streamers'][twitch_name]['title'] = jsondata['stream']['channel']['status']
+                                json.dump(data, f, indent=4)
 
-                        print(difference)
-                        print(jsondata['stream']['channel']['status'])
+                            print(difference)
+                            print(jsondata['stream']['channel']['status'])
 
-                        with open('created_at.json', 'w') as f:
-                            json.dump(data, f, indent=4)
-
-                return bcolors.OKCYAN + jsondata['stream']['channel']['display_name'] + " is live! " + jsondata['stream']['created_at'] + bcolors.ENDC
+                    return jsondata['stream']['channel']['display_name'] + " is live! " + jsondata['stream']['created_at']
             else:
-                    return bcolors.FAIL + twitch_name + " is not currently live." + bcolors.ENDC
-
+                return twitch_name + " is not live."
     except Exception as e:
         print("Error checking user: ", e)
         return False
-
 while True:
     for twitch_name in content_json["twitch_name"]:
         print(checkUser(twitch_name))
-    print(bcolors.OKGREEN + "\nFinsished a checkUser cycle, will wait 5 seconds until a new cycle will start.\n" + bcolors.ENDC)
-    sleep(5)
+    print("Finsished a checkUser cycle, will wait 3 seconds until a new cycle will start.\n")
+    sleep(3)
